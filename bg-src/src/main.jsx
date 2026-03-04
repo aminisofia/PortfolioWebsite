4aa938a95d3664bac914f5329f1c38261439a87b
Sofia Aminifard <aminisofia@icloud.com>
2026-03-03 23:55:52 -0800

    fixes

--- bg-src/src/main.jsx
+++ bg-src/src/main.jsx
@@ -6,12 +6,12 @@ ReactDOM.createRoot(document.getElementById('background-root')).render(
     <React.StrictMode>
         <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
             <Dither
-                waveColor={[0.8, 0.4, 0.7]}
+                waveColor={[0.7,0.5,0.8]}
                 disableAnimation={false}
-                enableMouseInteraction
-                mouseRadius={0.1}
+                enableMouseInteraction={false}
+                mouseRadius={0.3}
                 colorNum={2.5}
-                waveAmplitude={0.2}
+                waveAmplitude={0.3}
                 waveFrequency={3}
                 waveSpeed={0.02}
             />
